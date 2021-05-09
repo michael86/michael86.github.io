9ea5b8b6cd1ecd635bdfd2bdfc8dfeff9b063e9c
michael86 <michael8t6@gmail.com>
2021-05-09 22:48:08 +0100

    blrp-codebase -> Init commit

--- app.js
+++ app.js
@@ -1,63 +1,21 @@
-const userInput = document.querySelector('#user-input');
-const image = document.querySelector('#poke-img');
-const description = document.querySelector('#description');
-const pokeId = document.querySelector('#id');
-const pokeMoves = document.querySelector('#moves');
+const search = document.getElementById('search-form');
+const quote = document.getElementById('quote-data');
+const codeBlock = document.getElementById('code-box');
 
-userInput.addEventListener('submit', async function(e){
-    e.preventDefault();
+const updateCode = val => {
+    const searchTerm = val;
+    const data = code[searchTerm]
 
-    const pokemon = this.elements[0].value;
-    if(pokemon.length === 0) return;
+    quote.innerText = '';
+    quote.innerText = data.snippet;
 
-    const pokeData = await getPokemon(pokemon);
-    const pokeDesc = await getPokemonDescription(pokeData.species.url)
-    setImage(pokeData.sprites);
-    setDesc(pokeDesc);
-    setId(pokeData.id);
-    setMoves(pokeData.moves);
-});
+    codeBlock.textContent = '';
+    codeBlock.textContent = data.code.replace('\\', '');
 
-const getPokemon = async (pokemon) => {
-    try {
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-        return res.data;
-    } catch (e) {
-        console.log('Error getting pokemon', e);
-    };
-};
-
-const setImage = sprites => {
-    const url = sprites.other.dream_world.front_default ? sprites.other.dream_world.front_default : sprites.back_default;
-    image.src = url;
-};
-
-const getPokemonDescription = async url => {
-    try {
-        const res = await axios.get(url);
-        return res.data.flavor_text_entries[0].flavor_text;
-    } catch (e) {
-        console.log('Error getting pokemon description', e);
-    };
+    Prism.highlightElement(codeBlock);
 }
 
-const setDesc = desc => {
-    description.innerText = desc.replace(/\r?\n|\r/g, " ").replace('', ' ');
-};
-
-const setId = id => {
-    pokeId.innerText = id;
-};
-
-const setMoves = moves => {
-
-    pokeMoves.innerHTML = '';
-
-    for (let i = 0; i<=6; i++) {
-        const move = moves[i].move.name
-        const entry = document.createElement('li');
-        entry.append(move);
-        pokeMoves.append(entry);
-    }
-
-};
+search.addEventListener('submit', function (e) {
+    e.preventDefault()
+    updateCode(this.elements.search.value);
+});
---
+++ snippets.js
@@ -0,0 +1,41 @@
+const code = {
+    ['bt-target']: {
+        code: `local peds = {
+        \`mp_m_shopkeep_01\`,
+        \`a_m_y_hippy_01\`,
+        \`csb_chef\`,
+        \`a_f_y_hipster_02\`,
+        \`s_m_y_prisoner_01\`,
+        }
+
+AddTargetModel(peds, {
+    options = {
+        {
+            event = "esx_inventoryhud:openStore",
+            icon = "fas fa-dumpster",
+            label = "Purchase Items",
+            entityLocation = {
+            vector3(123.0, 456.0, 789.0),
+            vector3(987.0, 654.0, 321.0),
+        }
+    }},
+    distance = 2.5
+    }
+)`,
+        snippet: 'BT Target is used for the 3rd eye. It allows us to remove unnecersary constant distance checks. '
+    },
+    ['blrp']: {
+        code: `xPlayer.charId -> Will return this chars current id. Used for updating the database when used with xPlayer.identifier
+userData.name -> Will return this chars full rp name.
+userData.gender -> Will return this chars gender. Retval = 'm' or 'f'
+userData.dob -> Will return this chars date of birth. Retval = dd-mm-yyy
+TriggerEvent('esx:loadingScreenOff') --This is triggered once a player has chosen there char and their ped has been loaded.`,
+        snippet: 'Because ESX is weird and never had these as a base feature, I added it. We can now use the following.. charId will be required (along with identifier) to update that players current character.'
+    },
+    ['vehicles']: {
+        code: `local vehicle = CreateVehicle(config.vehicle, config.spawnLocation, config.spawnLocation.heading, 1, 1)
+TriggerServerEvent("carlock:issueTempKey", GetVehicleNumberPlateText(vehicle));`,
+        snippet: 'Due to vehicles being locked on spawn. We can utilize, "carlock:issueTempKey" to issue a temp key to a player. You need to pass over the vehicles license plate.'
+    }
+
+}
